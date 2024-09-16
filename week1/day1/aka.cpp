d181d94569d28ea16f8cc0f69eaa002f40a15e94
AkAnK1407 <121657871+AkAnK1407@users.noreply.github.com>
2024-09-16 22:52:54 +0530

    updated

--- week1/day1/aka.cpp
+++ week1/day1/aka.cpp
@@ -3,15 +3,13 @@ using namespace std;
 int main(){
     int n,s=0;
     cin>>n;
-    vector<int>v;
+    vector<int>v(n);
     for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        v.push_back(x); 
+        cin>>v[i];
     }
     for(auto i:v){
         s+=i;
     }
-    cout<<s<<endl;
-    return 0;
+    cout<<"sum = "<<s<<endl;
+    
 }
